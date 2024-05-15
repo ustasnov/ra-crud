1beeac2ca008355d1b1f16904ab4193330ea21ee
ustasnov <ustasnov@gmail.com>
2024-05-15 21:48:24 +0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Notes from './components/notes/Notes.jsx'
+import './main.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Notes />
+  </React.StrictMode>,
+)
+
